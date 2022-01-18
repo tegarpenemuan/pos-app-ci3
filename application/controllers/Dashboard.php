5efe638dd4b7beb61592cc4e6a1da27caa0e454a
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-01-18 23:27:18 +0700

    Implementasi Templating dan Page Dinamis

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,6 +5,6 @@ class Dashboard extends CI_Controller
 {
 	public function index()
 	{
-		$this->load->view('dashboard');
+		$this->template->load('template', 'dashboard');
 	}
 }
---
+++ application/controllers/Supplier.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Supplier extends CI_Controller
+{
+    public function index()
+    {
+        $this->template->load('template', 'supplier/supplier_data');
+    }
+}
