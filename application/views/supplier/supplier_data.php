29623e6587ec8aa447a3637528c9bfcf418bd99c
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-01-19 16:15:10 +0700

    Fitur Tampil Data dan Hapus Data Supllier

--- application/views/supplier/supplier_data.php
+++ application/views/supplier/supplier_data.php
@@ -1,1 +1,54 @@
-Supplier Data
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>Suppliers
+        <small>Pemasok Barang</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Suppliers</li>
+    </ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+
+    <div class="box">
+        <div class="box-header">
+            <h3 class="box-title">Data Suppliers</h3>
+            <div class="pull-right">
+                <a href="<?= site_url('supplier/add') ?>" class="btn btn-primary btn-flat"><i class="fa fa-user-plus"></i> Create</a>
+            </div>
+        </div>
+        <div class="box-body table-responsive">
+            <table class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Phone</th>
+                        <th>Address</th>
+                        <th>Description</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($row->result() as $key => $data) { ?>
+                        <tr>
+                            <td><?= $key + 1 ?>.</td>
+                            <td><?= $data->name ?></td>
+                            <td><?= $data->phone ?></td>
+                            <td><?= $data->address ?></td>
+                            <td><?= $data->description ?></td>
+                            <td class="text-center" width="160px" nowrap>
+                                <a href="<?= site_url('supplier/edit/' . $data->supplier_id) ?>" class="btn btn-primary btn-flat btn-xs"><i class="fa fa-pencil"></i> Update</a>
+                                <a href="<?= site_url('supplier/del/' . $data->supplier_id) ?>" onclick="return confirm('Yakin hapus data?');" class="btn btn-danger btn-flat btn-xs"><i class="fa fa-trash-o"></i> Update</a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+</section>
+<!-- /.content -->
