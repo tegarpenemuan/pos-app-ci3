243e53723bcdc0cb40dbf459046e47fe361fc65a
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-01-19 14:16:09 +0700

    Menambahkan Fitur Delete User Dengan Method POST

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -44,4 +44,15 @@ class User extends CI_Controller
             echo "<script>window.location='" . site_url('user') . "';</script>";
         }
     }
+
+    public function del()
+    {
+        $id = $this->input->post('user_id');
+        $this->user_m->del($id);
+
+        if ($this->db->affected_rows() > 0) {
+            echo "<script>alert('Data berhasil dihapus');</script>";
+        }
+        echo "<script>window.location='" . site_url('user') . "';</script>";
+    }
 }
