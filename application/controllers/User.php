0bd9003659790e74952127b000003f141e95823e
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-01-19 12:28:23 +0700

    Membuat Form Tambah Data Dengan Validasi CI

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -15,6 +15,23 @@ class User extends CI_Controller
 
     public function add()
     {
-        $this->template->load('template', 'user/user_form_add');
+        $this->load->library('form_validation');
+
+        $this->form_validation->set_rules('fullname', 'Name', 'required');
+        $this->form_validation->set_rules('username', 'Username', 'required|min_length[5]|is_unique[user.username]');
+        $this->form_validation->set_rules('password', 'Password', 'required|min_length[5]');
+        $this->form_validation->set_rules('passconf', 'Konfirmasi Password', 'required|matches[password]');
+        $this->form_validation->set_rules('level', 'Level', 'required');
+
+        $this->form_validation->set_message('required', '%s masih kosong, silahkan isi');
+        $this->form_validation->set_message('min_length', '{field} minimal 5 karakter');
+        $this->form_validation->set_message('matches', '%s tidak sesuai dengan password');
+        $this->form_validation->set_message('is_unique', '%s sudah dipakai, silahkan ganti');
+
+        if ($this->form_validation->run() == false) {
+            $this->template->load('template', 'user/user_form_add');
+        } else {
+            echo "proses simpan data user baru";
+        }
     }
 }
--- application/views/user/user_form_add.php
+++ application/views/user/user_form_add.php
@@ -1,1 +1,69 @@
-Tambah Data User
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>Users
+        <small>Pengguna</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Users</li>
+    </ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+
+    <div class="box">
+        <div class="box-header">
+            <h3 class="box-title">Add Users</h3>
+            <div class="pull-right">
+                <a href="<?= site_url('user') ?>" class="btn btn-danger btn-flat"><i class="fa fa-undo"></i> Back</a>
+            </div>
+        </div>
+
+        <div class="box-body">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <?php echo validation_errors(); ?>
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label for="name">Name *</label>
+                            <input type="text" name="fullname" class="form-control" id="name" value="<?= set_value("fullname") ?>" autocomplete="off">
+                        </div>
+                        <div class="form-group">
+                            <label for="username">Username *</label>
+                            <input type="text" name="username" class="form-control" id="username" value="<?= set_value("username") ?>" autocomplete="off">
+                        </div>
+                        <div class="form-group">
+                            <label for="pass">Password *</label>
+                            <input type="password" name="password" class="form-control" id="pass" value="<?= set_value("password") ?>" autocomplete="off">
+                        </div>
+                        <div class="form-group">
+                            <label for="cpass">Password Confirmation *</label>
+                            <input type="password" name="passconf" class="form-control" id="cpass" value="<?= set_value("passconf") ?>" autocomplete="off">
+                        </div>
+                        <div class="form-group">
+                            <label for="address">Address</label>
+                            <textarea name="address" id="address" cols="30" rows="10" class="form-control" autocomplete="off"><?= set_value("address") ?></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="level">Level *</label>
+                            <select name="level" class="form-control" id="level">
+                                <option value="">- Pilih -</option>
+                                <option value="1">Admin</option>
+                                <option value="2">Kasir</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-success btn-flat">
+                                <i class="fa fa-paper-plane"></i> Save
+                            </button>
+                            <button type="reset" class="btn btn-flat">Reset</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+<!-- /.content -->
