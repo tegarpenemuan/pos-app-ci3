fbb51b44fea9c7866849463d06f993c5dce6bc90
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-01-20 15:18:20 +0700

    Validasi Pada Data Barcode agar tidak kembar

--- application/controllers/Item.php
+++ application/controllers/Item.php
@@ -23,6 +23,7 @@ class item extends CI_Controller
         $item->barcode = null;
         $item->name = null;
         $item->price = null;
+        $item->category_id = null;
 
         $query_category = $this->category_m->get();
 
@@ -45,9 +46,19 @@ class item extends CI_Controller
     {
         $post = $this->input->post(null, TRUE);
         if (isset($_POST['add'])) {
-            $this->item_m->add($post);
+            if ($this->item_m->check_barcode($post['barcode'])->num_rows() > 0) {
+                $this->session->set_flashdata('error', "Barcode $post[barcode] sudah dipakai barang lain");
+                redirect('item/add');
+            } else {
+                $this->item_m->add($post);
+            }
         } else if (isset($_POST['edit'])) {
-            $this->item_m->edit($post);
+            if ($this->item_m->check_barcode($post['barcode'], $post['id'])->num_rows() > 0) {
+                $this->session->set_flashdata('error', "Barcode $post[barcode] sudah dipakai barang lain");
+                redirect('item/edit/' . $post['id']);
+            } else {
+                $this->item_m->edit($post);
+            }
         }
 
         if ($this->db->affected_rows() > 0) {
