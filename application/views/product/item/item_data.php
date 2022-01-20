5053b49e059e8cc6be54bf25ad1e1308c57b7edf
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-01-20 14:31:06 +0700

    Add Fitur Tambah Item & Penerapan Selectoption CI

---
+++ application/views/product/item/item_data.php
@@ -0,0 +1,50 @@
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>Items
+        <small>Data Barang</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Items</li>
+    </ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+    <?php $this->view('messages') ?>
+    <div class="box">
+        <div class="box-header">
+            <h3 class="box-title">Data Product Items</h3>
+            <div class="pull-right">
+                <a href="<?= site_url('item/add') ?>" class="btn btn-primary btn-flat"><i class="fa fa-plus"></i> Create Product Item</a>
+            </div>
+        </div>
+        <div class="box-body table-responsive">
+            <table class="table table-bordered table-striped" id="table1">
+                <thead>
+                    <tr>
+                        <th style="width: 5%;">#</th>
+                        <th>Barcode</th>
+                        <th>Name</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($row->result() as $key => $data) { ?>
+                        <tr>
+                            <td><?= $key + 1 ?>.</td>
+                            <td><?= $data->barcode ?></td>
+                            <td><?= $data->name ?></td>
+                            <td class="text-center" width="160px" nowrap>
+                                <a href="<?= site_url('item/edit/' . $data->item_id) ?>" class="btn btn-primary btn-flat btn-xs"><i class="fa fa-pencil"></i> Update</a>
+                                <a href="<?= site_url('item/del/' . $data->item_id) ?>" onclick="return confirm('Yakin hapus data?');" class="btn btn-danger btn-flat btn-xs"><i class="fa fa-trash-o"></i> Update</a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+</section>
+<!-- /.content -->
--- application/views/template.php
+++ application/views/template.php
@@ -160,7 +160,7 @@
                             <i class="fa fa-users"></i> <span>Customers</span>
                         </a>
                     </li>
-                    <li class="treeview <?= $this->uri->segment(1) == 'category' || $this->uri->segment(1) == 'unit' ? 'active' : '' ?>">
+                    <li class="treeview <?= $this->uri->segment(1) == 'category' || $this->uri->segment(1) == 'unit' || $this->uri->segment(1) == 'item' ? 'active' : '' ?>">
                         <a href="#">
                             <i class="fa fa-archive"></i><span>Products</span>
                             <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
@@ -168,7 +168,7 @@
                         <ul class="treeview-menu">
                             <li <?= $this->uri->segment(1) == 'category' ? 'class="active"' : '' ?>><a href="<?= site_url('category') ?>"><i class="fa fa-circle-o"></i> Categories</a></li>
                             <li <?= $this->uri->segment(1) == 'unit' ? 'class="active"' : '' ?>><a href="<?= site_url('unit') ?>"><i class="fa fa-circle-o"></i> Units</a></li>
-                            <li><a href="#"><i class="fa fa-circle-o"></i> Items</a></li>
+                            <li <?= $this->uri->segment(1) == 'item' ? 'class="active"' : '' ?>><a href="<?= site_url('item') ?>"><i class="fa fa-circle-o"></i> Items</a></li>
                         </ul>
                     </li>
                     <li class="treeview">
