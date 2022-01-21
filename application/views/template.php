d37d7e4ba4d0cacc240e8e29a671f68c34395333
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-01-21 22:09:50 +0700

    Membuat Desain Form Transaksi Penjualan

--- application/views/template.php
+++ application/views/template.php
@@ -29,7 +29,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 
-<body class="hold-transition skin-purple sidebar-mini">
+<body class="hold-transition skin-purple sidebar-mini <?= $this->uri->segment(1) == 'sale' ? 'sidebar-collapse' : null ?>">
     <!-- Site wrapper -->
     <div class="wrapper">
 
@@ -171,13 +171,13 @@
                             <li <?= $this->uri->segment(1) == 'item' ? 'class="active"' : '' ?>><a href="<?= site_url('item') ?>"><i class="fa fa-circle-o"></i> Items</a></li>
                         </ul>
                     </li>
-                    <li class="treeview <?= $this->uri->segment(1) == 'stock' ? 'active' : '' ?>">
+                    <li class="treeview <?= $this->uri->segment(1) == 'stock' || $this->uri->segment(1) == 'sale' ? 'active' : '' ?>">
                         <a href="#">
                             <i class="fa fa-shopping-cart"></i><span>Transaction</span>
                             <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
                         </a>
                         <ul class="treeview-menu">
-                            <li><a href="#"><i class="fa fa-circle-o"></i> Sales</a></li>
+                            <li <?= $this->uri->segment(1) == 'sale' ? 'class="active"' : '' ?>> <a href="<?= site_url('sale') ?>"><i class="fa fa-circle-o"></i> Sales</a></li>
                             <li <?= $this->uri->segment(1) == 'stock' && $this->uri->segment(2) == 'in' ? 'class="active"' : '' ?>><a href="<?= site_url('stock/in') ?>"><i class="fa fa-circle-o"></i> Stock In</a></li>
                             <li><a href="#"><i class="fa fa-circle-o"></i> Stock Out</a></li>
                         </ul>
